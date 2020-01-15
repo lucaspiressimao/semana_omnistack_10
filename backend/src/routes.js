0af64ba6f75f06528942cf7e8c7b9fa1815f99fe
Lucas Pires <lucaspiressimao@gmail.com>
2020-01-14 23:39:57 -0300

    backend adicionado tratamento de rotas para controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express')
+const DevController = require('./controllers/dev.js')
+const SearchController = require('./controllers/search.js')
 
 const routes = Router()
 
-routes.get('/', (request, response) => {
-    return response.send('OK')
-})
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store)
+
+routes.get('/search', SearchController.index)
 
 module.exports = routes
