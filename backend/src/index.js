e3f097ab432660f9bf276183e0e3fb571e26fc53
Lucas Pires <lucaspiressimao@gmail.com>
2020-01-14 23:24:03 -0300

    Adicionado mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const routes = require('./routes.js')
 
+mongoose.connect('mongodb+srv://admin:>@cluster0-5dgmx.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 const app = express()
-
 app.use(express.json())
-
 app.use(routes)
-
 app.listen(3000)
